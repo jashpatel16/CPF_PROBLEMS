eb3dfa397d4d81cb0f242985290e6ec2a316b464
jashpatel16 <jashapatel9972@gmail.com>
2026-07-23 22:47:43 +0530

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"Enter value of a:";
+    cin>>a;
+    cout<<"Enter value of b:";
+    cin>>b;
+    sum=a+b;
+    cout<<"SUM OF a and b is:"<<sum<<endl;
+    return 0;
+}

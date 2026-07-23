1636e9ee880454ba1580e65ad58fd75deb73a384
Jash Patel <jashapatel9972@gmail.com>
2026-07-23 22:04:23 +0530

    SOLUTIONS

---
+++ p1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = 5.5;
+    double c = 5.55555;
+    char d = 'J';
+    bool e = true;
+    cout<<"Integer:"<<a<<endl;
+    cout<<"Float:"<<b<<endl;
+    cout<<"Double:"<<c<<endl;
+    cout<<"Character:"<<d<<endl;
+    cout<<"Boolean:"<<e<<endl;
+    return 0;
+
+}

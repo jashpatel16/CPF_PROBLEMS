80c2bc18d366b1c00ab49182e95c61e5ab9185ba
jashpatel16 <jashapatel9972@gmail.com>
2026-07-24 11:07:20 +0530

    Add files via upload

---
+++ p12.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter ASCII value of first character:";
+    cin>>a;
+    cout<<"Enter ASCII value of second character:";
+    cin>>b;
+    cout<<"First Character:"<<char(a)<<endl;
+    cout<<"Second Character:"<<char(b)<<endl;
+    return 0;
+}
+

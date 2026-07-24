4e9d363ecc1fa71763cd9762d2c72495564accbe
jashpatel16 <jashapatel9972@gmail.com>
2026-07-24 11:38:44 +0530

    Add files via upload

---
+++ p13.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    float Result;
+    cout<<"Enter Numerator:";
+    cin>>a;
+    cout<<"Enter Denominator:";
+    cin>>b;
+    Result=float(a)/float(b);
+    cout<<"RESULT:"<<Result<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 

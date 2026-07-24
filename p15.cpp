370dd03a0aead7c62fd98c6ba1e6f857e092e493
jashpatel16 <jashapatel9972@gmail.com>
2026-07-24 12:49:47 +0530

    Add files via upload

---
+++ p15.cpp
@@ -0,0 +1,24 @@
+//STUDENT's INFORMATION//
+#include<iostream>
+using namespace std;
+int main(){ 
+    cout<<"******* STUDENT'S INFORMATION *******"<<endl;
+    string name = "JASH PATEL";
+    int Roll_No = 10;
+    float marks = 85.5;
+    cout<<"The Student's Name is: "<<name<<endl;
+    cout<<"The Student's Roll Number is: "<<Roll_No<<endl;
+    cout<<"The Student's Marks are: "<<marks<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
